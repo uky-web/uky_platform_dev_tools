2291cefe14c6ece209a3c17d8ddc5e93ab135464
jason.ullstam <jason.ullstam@gmail.com>
2021-12-02 15:58:04 -0500

    permissions work

--- permissionfix.sh
+++ permissionfix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod 775 ../../../.ddev/gitpod-setup-ddev.sh
-chmod 775 ../../../.ddev/run-phpstorm.sh
-chmod 775 ../../../.ddev/uk-site-install.sh
+chmod 755 ../../../.ddev/gitpod-setup-ddev.sh
+chmod 755 ../../../.ddev/run-phpstorm.sh
+chmod 755 ../../../.ddev/uk-site-install.sh
 
 
