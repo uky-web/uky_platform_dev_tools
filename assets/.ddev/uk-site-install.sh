ef8e619eeb498a483c3e27fe2109ec2cfb00b2f2
Jason Ullstam <jason.ullstam@uky.edu>
2022-04-26 18:17:12 +0000

    Update uk-site-install.sh

--- assets/.ddev/uk-site-install.sh
+++ assets/.ddev/uk-site-install.sh
@@ -11,7 +11,7 @@ if [ -f "reference/sanitized.sql" ]
     ddev drush cr
     ddev drush cim -y
     ddev drush cr
-    ddev composer update
+    composer update
     ddev drush cr
     ddev drush user:create admin --password="admin"
     ddev drush urol "administrator" admin
@@ -21,10 +21,9 @@ if [ -f "reference/sanitized.sql" ]
         siteurl=$(cat "$file")
         ddev drush cr
         ddev drush en stage_file_proxy
-        ddev drush cex -y
+        ddev drush cr
         ddev drush config-set stage_file_proxy.settings origin $siteurl -y
-        ddev drush config-set stage_file_proxy.settings hotlink true -y
-        ddev drush cim -y
+        #ddev drush config-set stage_file_proxy.settings hotlink true -y
         ddev drush cr
         echo "Stage File Proxy enabled and configured..."
     fi
